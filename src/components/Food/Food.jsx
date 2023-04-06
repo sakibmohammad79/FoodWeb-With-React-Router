135ac1530edb9c31e76625918e492815dfb7ce3d
Mohammad Sakib <mohammadsakib7679@gmail.com>
2023-04-06 20:07:16 +0600

    data display and make card

--- src/components/Food/Food.jsx
+++ src/components/Food/Food.jsx
@@ -4,8 +4,17 @@ const Food = ({food}) => {
     console.log(food);
 
     return (
-        <div>
-            
+        <div className='bg-slate-100 drop-shadow-lg p-5'>
+            <img className='text-center' src={food.strCategoryThumb} alt="" />
+            <div className='flex justify-between items-center'>
+            <div>
+            <p className='font-bold text-2xl'> {food.strCategory}</p>
+            <p className='font-bold text-2xl mt-2'>Id: {food.idCategory}</p>
+            </div>
+            <div>
+                <button className='bg-orange-600 hover:bg-orange-800 text-white font-bold text-lg rounded-xl py-2 px-6'>Details</button>
+            </div>
+            </div>
         </div>
     );
 };
--- src/components/Foods/Foods.jsx
+++ src/components/Foods/Foods.jsx
@@ -5,7 +5,7 @@ import Food from '../Food/Food';
 const Foods = () => {
     const Foods = useLoaderData();
     return (
-        <div>
+        <div className='md:grid grid-cols-3 gap-3 md:gap-8 mt-6 mx-3 md:mx-12'>
             {
                 Foods.categories.map((food) => <Food
                  key={food.idCategory}
